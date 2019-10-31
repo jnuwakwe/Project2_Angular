c4028d4981963eb26bc7a65b3e9ad1cd1703fcec
Uwakwe <jnuwakwe@gmail.com>
2019-10-30 21:15:41 -0400

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,87 @@
+// import { BrowserModule } from '@angular/platform-browser';
+// import { NgModule } from '@angular/core';
+
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+// import { HomeComponent } from './home/home.component';
+// import { LoginComponent } from './login/login.component';
+// import { SignupComponent } from './signup/signup.component';
+// import { ProfileComponent } from './profile/profile.component';
+
+
+import { BrowserModule } from '@angular/platform-browser';  
+import { NgModule } from '@angular/core';  
+  
+// import Http module  
+import { HttpModule} from '@angular/http';  
+  
+// import ReactiveFormsModule for reactive form  
+import { ReactiveFormsModule } from '@angular/forms';  
+  
+// import module for Routing.  
+import { RouterModule } from '@angular/router';  
+  
+import { AppComponent } from './app.component';  
+import { LoginComponent } from './login/login.component';  
+import { HomeComponent } from './home/home.component';  
+import { SignupComponent } from './signup/signup.component';  
+import { AdminService } from './services/admin.service';  
+import { ProfileComponent } from './profile/profile.component'; 
+
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     HomeComponent,
+//     LoginComponent,
+//     SignupComponent,
+//     ProfileComponent
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
+
+
+@NgModule({  
+  declarations: [  
+    AppComponent,  
+    LoginComponent,  
+    HomeComponent,  
+    SignupComponent,  
+    ProfileComponent  
+  ],  
+  imports: [  
+    BrowserModule,  
+    ReactiveFormsModule,  
+    HttpModule,  
+    RouterModule.forRoot([  
+      {  
+        path : '',  
+        component : HomeComponent   
+      },  
+      {  
+        path : 'login',  
+        component : LoginComponent    
+      },  
+      {  
+        path : 'signup',  
+        component : SignupComponent   
+      },  
+      {  
+        path : 'profile/:adminId',  
+        component : ProfileComponent  
+      }  
+    ])  
+  
+  ],  
+  providers: [  
+    AdminService  
+  ],  
+  bootstrap: [AppComponent]  
+})  
+export class AppModule { }  
